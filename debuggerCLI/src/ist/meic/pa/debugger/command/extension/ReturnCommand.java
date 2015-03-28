365099502af91fd256a789db906119cdab391b81
NunoXu <nuno.xu@gmail.com>
2015-03-28 17:56:09 +0000

    Updated ReturnCommand extended with refactor

--- debuggerCLI/src/ist/meic/pa/debugger/command/extension/ReturnCommand.java
+++ debuggerCLI/src/ist/meic/pa/debugger/command/extension/ReturnCommand.java
@@ -1,19 +1,21 @@
 package ist.meic.pa.debugger.command.extension;
 
-import ist.meic.pa.command.common.FieldFinder;
 import ist.meic.pa.command.common.extension.ObjectContructorFromString;
 import ist.meic.pa.command.exception.CommandException;
-import ist.meic.pa.command.exception.NonPrimitiveReturnException;
 import ist.meic.pa.command.exception.WrongNumberOfArgumentsException;
 import ist.meic.pa.debugger.DebuggerCLIStackManager;
 import ist.meic.pa.debugger.MethodPrint;
 import ist.meic.pa.debugger.command.ReturnableCommand;
 
-import java.lang.reflect.Method;
-
 public class ReturnCommand extends ReturnableCommand {
 
 	private static final String COMMAND_NAME = "Return";
+
+	@Override
+	public void execute(String[] args, Throwable exception, Object target)
+			throws CommandException, Throwable {
+		execute(args, exception, target.getClass());
+	}
 
 	@Override
 	public void execute(String[] args, Throwable exception, Class<?> targetClass)
@@ -21,102 +23,29 @@ public class ReturnCommand extends ReturnableCommand {
 
 		MethodPrint lastCalledMethod = DebuggerCLIStackManager
 				.getMostRecentMethodCall();
-		String methodName = lastCalledMethod.getMethodName();
-
-				
-		// Warning: If it doesn't find the method it will result in a
-		// nullpointer exception
-		Method method = null;
-		for (Method m : FieldFinder.getDeclaredMethods(targetClass)){
-			if (m.getName().equals(methodName)) {
-				method = m;
-				break;
-			}
-
-		}
-		boolean originalAccessibleValue = method.isAccessible();
-		
-		method.setAccessible(true);
-		Class<?> returnType = method.getReturnType();
-		method.setAccessible(originalAccessibleValue);
-		
+		Class<?> returnType = lastCalledMethod.getReturnType();
 
 		if (returnType == void.class) {
-			if(args.length == 1)
-				_result = null;
-			else
+			if (args.length > 1) {
 				throw new WrongNumberOfArgumentsException(0, args.length - 1);
-
-		} else {
-			
-			
-			if(args.length == 2){
-				ObjectContructorFromString c = new ObjectContructorFromString(returnType, args[1]);
-				Object returnObj = c.convert();
-				_result = returnObj;
-			}else{
-				throw new WrongNumberOfArgumentsException(1, args.length - 1);
+			} else {
+				_result = null;
+				return;
 			}
-				
-				String returnValueString = args[1];
-			executeReturn(targetClass, returnValueString);
-			
-		}
-	}
-
-	@Override
-	public void execute(String[] args, Throwable exception, Object target)
-			throws CommandException, Throwable {
-
-		
-		execute(args, exception, target.getClass());
-		
-		//FIXME:Nao sei se faz sentido
-		/*try {
-
-			/*
-			 * if(args.length != 2){ throw new
-			 * WrongNumberOfArgumentsException(1, args.length); }
-			 *
-
-			String returnValueString = args[1];
-			Class<?> targetClass = target.getClass();
-
-			executeReturn(targetClass, returnValueString);
-
-		} catch (IllegalArgumentException | SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-	}
-
-	private void executeReturn(Class<?> targetClass, String returnValueString) throws CommandException {
-		MethodPrint lastCalledMethod = DebuggerCLIStackManager
-				.getMostRecentMethodCall();
-		String methodPrefix = targetClass.getName() + ".";
-		String methodFullName = lastCalledMethod.getMethodName();
-		String methodName = methodFullName.replace(methodPrefix, "");
-
-		Method method = null;
-		/* overloading methods always returns the same type */
-		for (Method m : targetClass.getDeclaredMethods()) {
-			if (m.getName().equals(methodName)) {
-				method = m;
-				break;
-			}
-
 		}
 
-		boolean originalAccessibleValue = method.isAccessible();
+		executeNonVoidReturn(args, returnType);
+	}
 
-		method.setAccessible(true);
+	private void executeNonVoidReturn(String[] args, Class<?> returnType)
+			throws CommandException {
 
-		Class<?> returnClass = method.getReturnType();
-		ObjectContructorFromString c = new ObjectContructorFromString(returnClass, returnValueString);
+
+		if (args.length > 2)
+			throw new WrongNumberOfArgumentsException(1, args.length - 1);
+
+		ObjectContructorFromString c = new ObjectContructorFromString(returnType, args[1]);
 		Object returnObj = c.convert();
-
-		method.setAccessible(originalAccessibleValue);
-
 		_result = returnObj;
 	}
 
