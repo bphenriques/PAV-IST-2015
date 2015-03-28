8468ffbd2ed55f708a0cc03024ab9cf7fe2a8e0b
GitTiago <tiagomanuelfs@hotmail.com>
2015-03-28 19:13:44 +0000

    Return fixed for no arguments provided in non-void function and Javadoc for it added

--- debuggerCLI/src/ist/meic/pa/debugger/command/simple/ReturnCommand.java
+++ debuggerCLI/src/ist/meic/pa/debugger/command/simple/ReturnCommand.java
@@ -8,17 +8,33 @@ import ist.meic.pa.debugger.command.ReturnableCommand;
 import ist.meic.pa.debugger.stack.StackElement;
 import ist.meic.pa.debugger.stack.StackManager;
 
+/**
+ * The Class ReturnCommand provides the possibility of returning a function with
+ * a given value.
+ */
 public class ReturnCommand extends ReturnableCommand {
 
+	/** The Constant COMMAND_NAME. */
 	private static final String COMMAND_NAME = "Return";
 
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see ist.meic.pa.debugger.command.Command#execute(java.lang.String[],
+	 * java.lang.Throwable, java.lang.Object)
+	 */
 	@Override
 	public void execute(String[] args, Throwable exception, Object target)
 			throws CommandException, Throwable {
 		execute(args, exception, target.getClass());
 	}
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see ist.meic.pa.debugger.command.Command#execute(java.lang.String[],
+	 * java.lang.Throwable, java.lang.Class)
+	 */
 	@Override
 	public void execute(String[] args, Throwable exception, Class<?> targetClass)
 			throws CommandException, Throwable {
@@ -26,33 +42,49 @@ public class ReturnCommand extends ReturnableCommand {
 		StackElement lastCalledMethod = StackManager.getMostRecentMethodCall();
 		Class<?> returnType = lastCalledMethod.getReturnType();
 
-		if (returnType == void.class){
-			if(args.length > 1){
+		if (returnType == void.class) {
+			if (args.length != 1) {
 				throw new WrongNumberOfArgumentsException(0, args.length - 1);
-			}else{
+			} else {
 				_result = null;
 				return;
 			}
 		}
-		
+
 		executeNonVoidReturn(args, returnType);
 	}
 
+	/**
+	 * Execute non void return.
+	 *
+	 * @param args
+	 *            the args
+	 * @param returnType
+	 *            the return type
+	 * @throws NonPrimitiveReturnException
+	 *             the non primitive return exception
+	 * @throws WrongNumberOfArgumentsException
+	 *             the wrong number of arguments exception
+	 */
 	private void executeNonVoidReturn(String[] args, Class<?> returnType)
 			throws NonPrimitiveReturnException, WrongNumberOfArgumentsException {
-		
-		if(!returnType.isPrimitive())
+
+		if (!returnType.isPrimitive())
 			throw new NonPrimitiveReturnException();
-		
-		if(args.length > 2)
+
+		if (args.length != 2)
 			throw new WrongNumberOfArgumentsException(1, args.length - 1);
-		
+
 		ObjectContructorFromString c = new ObjectContructorFromString();
 		Object returnObj = c.convert(returnType, args[1]);
 		_result = returnObj;
 	}
 
-	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see ist.meic.pa.debugger.command.Command#getCommandName()
+	 */
 	@Override
 	public String getCommandName() {
 		return COMMAND_NAME;
