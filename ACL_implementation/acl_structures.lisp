69ce066959404bdc29b8c02e501430da2e3b5db0
Bruno Alexandre Pires Henriques <ist172913@linbase.rnl.tecnico.ulisboa.pt>
2015-04-30 19:17:01 +0100

    Temporary

--- ACL_implementation/acl_structures.lisp
+++ ACL_implementation/acl_structures.lisp
@@ -1,6 +1,30 @@
+;(defmethod print-tensor ((tens tensor-vector) stream depth)
+;	(declare (ignore depth))
+;	(let* ((content (tensor-vector-cntent tens))
+;		   (len (array-dimension content 0)))
+;		   (dotimes (l len)
+;				(format stream "~D " (aref content l)))))
+				
+
+(defstruct (tensor 
+				(:print-function 
+					(lambda (struct stream depth)
+						(declare (ignore struct stream depth))
+						(error "ERROR: No printing mechanism for default tensor"))))
+	content)
+
+(defstruct (tensor-scalar 
+				(:include tensor)
+				(:print-function 
+					(lambda (struct stream depth)
+						(declare (ignore depth))
+						(format stream "~D " (tensor-scalar-content struct))))))
+						
+
 ;create a scalar
-(defun s ())
+(defun s (value)
+	(make-tensor-scalar :content value))
 
 ;create a vectors
-(defun v ())
-
+;(defun v (&rest values)
+;	(make-tensor-vector :content values))
