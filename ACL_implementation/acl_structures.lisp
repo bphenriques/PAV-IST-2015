2ced76bb79d88b8a44fbc62fa4001ede28565197
Bruno Alexandre Pires Henriques <ist172913@linbase.rnl.tecnico.ulisboa.pt>
2015-04-30 20:08:50 +0100

    Temporary commit

--- ACL_implementation/acl_structures.lisp
+++ ACL_implementation/acl_structures.lisp
@@ -1,30 +1,41 @@
-;(defmethod print-tensor ((tens tensor-vector) stream depth)
-;	(declare (ignore depth))
-;	(let* ((content (tensor-vector-cntent tens))
-;		   (len (array-dimension content 0)))
-;		   (dotimes (l len)
-;				(format stream "~D " (aref content l)))))
-				
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Printing mechanims
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defstruct (tensor 
-				(:print-function 
-					(lambda (struct stream depth)
-						(declare (ignore struct stream depth))
-						(error "ERROR: No printing mechanism for default tensor"))))
-	content)
+(defun print-tensor-scalar (tensor stream)
+	(format stream "~D " (tensor-scalar-content tensor)))
+
+(defun print-tensor-vector (tens stream)
+	(let* ((content (tensor-vector-content tens))
+		   (len (array-dimension content 0)))
+		   (dotimes (l len)
+				(format stream "~D " (aref content l)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Structures
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defstruct tensor content)
 
 (defstruct (tensor-scalar 
-				(:include tensor)
-				(:print-function 
-					(lambda (struct stream depth)
-						(declare (ignore depth))
-						(format stream "~D " (tensor-scalar-content struct))))))
-						
+				(:include tensor))
+				(:print-object (lambda (tensor stream) 
+									(format t "Calling scalar")
+									(print-tensor-scalar tensor stream))))
+
+(defstruct (tensor-vector 
+				(:include tensor))
+				(:print-object (lambda (tensor stream)
+									(format t "Calling vector")
+									(print-tensor-vector tensor stream))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Public functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;create a scalar
 (defun s (value)
 	(make-tensor-scalar :content value))
 
-;create a vectors
-;(defun v (&rest values)
-;	(make-tensor-vector :content values))
+(defun v (&rest values)
+	(make-tensor-vector :content values))
