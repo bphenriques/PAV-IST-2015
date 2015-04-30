9453194054d9086ed7ede43bf2e88f3899cd74b4
bphenriques <brunoaphenriques@gmail.com>
2015-04-30 21:25:01 +0100

    Temporary commit with generics

--- ACL_implementation/acl_structures.lisp
+++ ACL_implementation/acl_structures.lisp
@@ -3,29 +3,27 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defgeneric print-tensor (tensor stream)
-	(:method ((tensor tensor) (stream stream))
-		(error "ERROR........")))
+	(:method ((tensor t) (stream t))
+		(error "ERRRRRORRRR")))
 
-(defmethod print-tensor ((tensor tensor-scalar) (stream stream))
+(defmethod print-sensor ((tensor tensor-scalar) (stream stream))
 	(format stream "~D " (tensor-scalar-content tensor)))
 
-
-(defstruct (tensor 
-				(:print-object (lambda (tensor stream)
-						(print-tensor tensor stream))))
-				content)
-
-
-(defstruct (tensor-scalar 
-				(:include tensor)))
-
-(defstruct (tensor-vector 
-				(:include tensor))
-				(:print-object (lambda (tensor stream)
-									(let* ((content (tensor-vector-content tensor))
+(defmethod print-sensor ((tensor tensor-vector) (stream stream))
+	(let* ((content (tensor-vector-content tensor))
 										   (len (array-dimension content 0)))
 									   (dotimes (l len)
-											(format stream "~D " (aref content l)))))))
+											(format stream "~D " (aref content l)))))
+
+(defstruct (tensor-scalar
+				(:print-object (lambda (tensor stream)
+									(print-sensor tensor stream))))
+				content)
+
+(defstruct (tensor-vector
+				(:print-object (lambda (tensor stream)
+									(print-sensor tensor stream))))
+				content)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Public functions
