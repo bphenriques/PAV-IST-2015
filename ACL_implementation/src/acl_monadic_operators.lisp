896cf50966ea76f8af3b3449771f510d1ce553ba
Bruno Alexandre Pires Henriques <ist172913@linbase.rnl.tecnico.ulisboa.pt>
2015-05-06 18:53:39 +0100

    Scan in progress...

--- ACL_implementation/src/acl_monadic_operators.lisp
+++ ACL_implementation/src/acl_monadic_operators.lisp
@@ -1,14 +1,28 @@
 ; Accepts a function and returns another function that, given a vecotr, computes the application of the function to sucessive elements of the vector.
 (defgeneric fold (func)
 	(:method ((func t))
-		(error "Bad argument given")))
+		(error "Argument is not a function")))
 
 (defmethod fold ((func function))
-	(lambda (vec) (reduce func (map 'list (lambda (x) x) (tensor-content vec))))) 
+	(lambda (vec) (reduce func (map 'list (lambda (x) x) (tensor-content vec)))))
 	
 
+
+(defgeneric scan (func)
+	(:method ((func t))
+		(error "Argument is not a function")))
+
 ; Similar to fold but using increasingly large subsets of the eleemnts of the vector, starting from a subset containg just the first element up to a subset containing all elements
-(defun scan ())
+;(defun scan ((func function))
+;	(lambda (vec) 
+;		(let* ((lst (map 'list (lambda (x) x) (tensor-content vec)))
+;			   (len (length lst)))
+;			   
+;			   nthcdr n list!!
+;			(dotimes (n len)
+				(
+		
+)
 
 ; Accepts a function and returns another functions taht, given two tensors, returns a new tensor with the result of applying the function to every combination of values from the first and second tesnsors
 (defun outer_product ())
