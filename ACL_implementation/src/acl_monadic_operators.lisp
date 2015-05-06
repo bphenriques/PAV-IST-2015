c21fb77c281613dfcf58cc72c86572737b37bad8
Bruno Alexandre Pires Henriques <ist172913@linbase.rnl.tecnico.ulisboa.pt>
2015-05-06 20:08:49 +0100

    started outer_product

--- ACL_implementation/src/acl_monadic_operators.lisp
+++ ACL_implementation/src/acl_monadic_operators.lisp
@@ -25,6 +25,26 @@
 				(apply #'v result))))
 
 ; Accepts a function and returns another functions taht, given two tensors, returns a new tensor with the result of applying the function to every combination of values from the first and second tesnsors
-(defun outer_product ())
+;(defgeneric outer_product (func)
+	;(:method ((func t))
+		;(error "Argument is not a function")))
 
-;(funcall (scan #'.+) (v 1 2 3))
+;(defmethod outer_product ((func function)) 
+	;(lambda (t1 t2)
+		;1))
+		
+;(defmethod outer_product ((func function)) 
+	;(lambda (t1 t2)
+		;1))
+	
+;;; PASSAR PARA OUTRO FICHEIRO DEPOIS
+
+;(defgeneric combination_tensor (t1 t2)
+	;(:method ((t1 t) (t2 t))
+		;(error "Arguments are not a tensor!")))
+		
+;(defgeneric combination_tensor (t1 t2)
+	;(:method ((t1 (tensor-scalar) (t2 t))
+		;(error "Arguments are not a tensor!")))
+
+	
--- ACL_implementation/src/util.lisp
+++ ACL_implementation/src/util.lisp
@@ -51,5 +51,15 @@
     (make-array (array-dimension arr 1) 
       :displaced-to arr 
        :displaced-index-offset (* row (array-dimension arr 1))))
+       
+       
+;;;;; RETIRADO NA NET - AINDA TENHO QUE PERCEBER E VER SE FUNCIONA - https://stackoverflow.com/questions/14758218/two-element-combinations-of-the-elements-of-a-list-inside-lisp-without-duplicat
+
+(defun pair-with (elem lst)
+  (mapcar (lambda (a) (list elem a)) lst))
+
+(defun unique-pairs (lst)
+  (mapcon (lambda (rest) (pair-with (car rest) (cdr rest)))
+          (remove-duplicates lst)))
         
     
