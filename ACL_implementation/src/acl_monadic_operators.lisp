ce36a85fc91f1b7136403b550d24ae353af0a755
Bruno Alexandre Pires Henriques <ist172913@linbase.rnl.tecnico.ulisboa.pt>
2015-05-06 19:44:55 +0100

    added scan

--- ACL_implementation/src/acl_monadic_operators.lisp
+++ ACL_implementation/src/acl_monadic_operators.lisp
@@ -4,7 +4,8 @@
 		(error "Argument is not a function")))
 
 (defmethod fold ((func function))
-	(lambda (vec) (reduce func (map 'list (lambda (x) x) (tensor-content vec)))))
+	(lambda (vec) 
+		(reduce func (map 'list (lambda (x) x) (tensor-content vec)))))
 	
 
 
@@ -13,16 +14,17 @@
 		(error "Argument is not a function")))
 
 ; Similar to fold but using increasingly large subsets of the eleemnts of the vector, starting from a subset containg just the first element up to a subset containing all elements
-;(defun scan ((func function))
-;	(lambda (vec) 
-;		(let* ((lst (map 'list (lambda (x) x) (tensor-content vec)))
-;			   (len (length lst)))
-;			   
-;			   nthcdr n list!!
-;			(dotimes (n len)
-				(
-		
-)
+(defmethod scan ((func function))
+	(lambda (vec)
+		(let* ((lst (map 'list (lambda (x) x) (tensor-content vec)))
+			   (len (length lst))
+			   (result (list)))
+			   (dotimes (l len)
+					(setf result (cons (reduce func lst) result))
+					(setf lst (butlast lst)))
+				(apply #'v result))))
 
 ; Accepts a function and returns another functions taht, given two tensors, returns a new tensor with the result of applying the function to every combination of values from the first and second tesnsors
 (defun outer_product ())
+
+;(funcall (scan #'.+) (v 1 2 3))
