e80970ec73823d9e52b52f103813e5f1e2488ef2
bphenriques <brunoaphenriques@gmail.com>
2015-05-08 18:41:42 +0100

    started outer_product

--- ACL_implementation/src/acl_monadic_operators.lisp
+++ ACL_implementation/src/acl_monadic_operators.lisp
@@ -24,20 +24,18 @@
 				(apply #'v result))))
 
 ; Accepts a function and returns another functions taht, given two tensors, returns a new tensor with the result of applying the function to every combination of values from the first and second tesnsors
-;(defgeneric outer_product (func)
-	;(:method ((func t))
-		;(error "Argument is not a function")))
+(defgeneric outer_product (func)
+	(:method ((func t))
+		(error "Argument is not a function")))
 
-;(defmethod outer_product ((func function)) 
-	;(lambda (t1 t2)
-		;1))
-		
-;(defmethod outer_product ((func function)) 
-	;(lambda (t1 t2)
-		;1))
-	
-;;; PASSAR PARA OUTRO FICHEIRO DEPOIS
+(defmethod outer_product ((func function)) 
+	(lambda (t1 t2)
+		(let* ((dim1 (tensor-dimensions t1))
+			   (dim2 (tensor-dimensions t2))
+			   (cont1 (make-array (length dim1 :initial-content 0)))
+			   (cont2 (make-array (length dim2 :initial-content 0))))
 
+			
 ;(defgeneric combination_tensor (t1 t2)
 	;(:method ((t1 t) (t2 t))
 		;(error "Arguments are not a tensor!")))
