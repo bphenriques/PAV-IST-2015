a8490eecec5c76e0e210902239acd4c458accd92
bphenriques <brunoaphenriques@gmail.com>
2015-05-08 11:50:53 +0100

    Started exercises.lisp

--- ACL_implementation/src/exercises.lisp
+++ ACL_implementation/src/exercises.lisp
@@ -1,21 +1,27 @@
 ; given a tensor, returns a scalar with the number of elements of the tensor
 (defun tally (tensor)
-	(let ((count 0))
-		(funcall (scan (lambda (x) (incf count))) tensor)
-		count))
+	(funcall (fold #'.+) (.not (.* (s 0) tensor))))
 
 ;given a tensor, returns a scalar with the number of dimensions of the tensor
-(defun rank ())
-
+(defun rank (tensor)
+	(funcall (fold #'.+) (.not (.* (s 0) (shape tensor)))))
 
 ; Define the function within that, given a vector of numbers v and two
 ; numbers n1 and n2, returns a vector containing only the elements of v
 ; that are in the range between n1 and n2
 
-(defun within ())
+(defun within (tensor n1 n2))
+
 
 ; given a tensor, returns a vector containing all the elements of the tensor
-(defun ravel ())
+(defun ravel (tensor))
 
 ;given a scalar, returns a vector with all prime numbers from 2 up to the scalar, inclusive
-(defun prime ())
+(defun prime (n1)
+	;(labels ((primep (n)
+	;		(cond ((= n 3) t)
+	;			  ((evenp n) nil)
+	;			   t
+;
+;
+;		)))
